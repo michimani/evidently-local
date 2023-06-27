c622c7a818961393d5056ac46cb5a3e6caaafe2f
michimani <michimani210@gmail.com>
2023-06-27 23:17:53 +0900

    add logger #1

---
+++ logger/logger_test.go
@@ -0,0 +1,173 @@
+package logger_test
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"michimani/evidentlylocal/logger"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewEvidentlyLocalLogger(t *testing.T) {
+	cases := []struct {
+		name      string
+		out       io.Writer
+		wantError bool
+	}{
+		{
+			name:      "success",
+			out:       os.Stdout,
+			wantError: false,
+		},
+		{
+			name:      "fail",
+			out:       nil,
+			wantError: true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			asst := assert.New(tt)
+			l, err := logger.NewEvidentlyLocalLogger(c.out)
+
+			if c.wantError {
+				asst.Nil(l)
+				asst.Error(err)
+				return
+			}
+
+			asst.NoError(err)
+			asst.NotNil(l)
+		})
+	}
+}
+
+func Test_ELLogger_Info(t *testing.T) {
+	cases := []struct {
+		name   string
+		msg    string
+		expect string
+	}{
+		{
+			name:   "success",
+			msg:    "test",
+			expect: `"message":"test"`,
+		},
+		{
+			name:   "success: empty message",
+			msg:    "",
+			expect: "",
+		},
+	}
+
+	out := bytes.Buffer{}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			tt.Cleanup(func() {
+				out.Reset()
+			})
+
+			asst := assert.New(tt)
+			l, err := logger.NewEvidentlyLocalLogger(&out)
+			asst.NoError(err)
+			asst.NotNil(l)
+
+			l.Info(c.msg)
+
+			asst.Contains(out.String(), `"level":"info"`, out.String())
+			asst.Contains(out.String(), c.expect, out.String())
+		})
+	}
+}
+
+func Test_ELLogger_Warn(t *testing.T) {
+	cases := []struct {
+		name   string
+		msg    string
+		expect string
+	}{
+		{
+			name:   "success",
+			msg:    "test",
+			expect: `"message":"test"`,
+		},
+		{
+			name:   "success: empty message",
+			msg:    "",
+			expect: "",
+		},
+	}
+
+	out := bytes.Buffer{}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			tt.Cleanup(func() {
+				out.Reset()
+			})
+
+			asst := assert.New(tt)
+			l, err := logger.NewEvidentlyLocalLogger(&out)
+			asst.NoError(err)
+			asst.NotNil(l)
+
+			l.Warn(c.msg)
+
+			asst.Contains(out.String(), `"level":"warn"`, out.String())
+			asst.Contains(out.String(), c.expect, out.String())
+		})
+	}
+}
+
+func Test_ELLogger_Error(t *testing.T) {
+	cases := []struct {
+		name    string
+		msg     string
+		err     error
+		expects []string
+	}{
+		{
+			name:    "success",
+			msg:     "test",
+			expects: []string{`"message":"test"`},
+		},
+		{
+			name:    "success: empty message",
+			msg:     "",
+			expects: []string{},
+		},
+		{
+			name:    "success with error",
+			msg:     "test",
+			err:     errors.New("test error"),
+			expects: []string{`"message":"test"`, `"error":"test error"`},
+		},
+	}
+
+	out := bytes.Buffer{}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			tt.Cleanup(func() {
+				out.Reset()
+			})
+
+			asst := assert.New(tt)
+			l, err := logger.NewEvidentlyLocalLogger(&out)
+			asst.NoError(err)
+			asst.NotNil(l)
+
+			l.Error(c.msg, c.err)
+
+			asst.Contains(out.String(), `"level":"error"`, out.String())
+			for _, expect := range c.expects {
+				asst.Contains(out.String(), expect, out.String())
+			}
+		})
+	}
+}
