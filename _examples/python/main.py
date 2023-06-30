588ef6726e17e3c56a94bcea1b75e25ae642b72a
michimani <michimani210@gmail.com>
2023-06-30 09:31:02 +0900

    fix python example #1

--- _examples/python/main.py
+++ _examples/python/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import uuid
 from typing import Final
 
@@ -7,32 +8,43 @@ from botocore.config import Config
 
 EVIDENTLY_ENDPOINT_URL: Final[str] = os.environ.get("EVIDENTLY_ENDPOINT_URL", "")
 
-# create Evidently client
-if len(EVIDENTLY_ENDPOINT_URL) == 0:
-    client = boto3.client("evidently")
-else:
-    client = boto3.client(
+
+def create_client():
+    """Create a CloudWatch Evidently client."""
+    if len(EVIDENTLY_ENDPOINT_URL) == 0:
+        return boto3.client("evidently")
+
+    return boto3.client(
         "evidently",
         endpoint_url=EVIDENTLY_ENDPOINT_URL,
         config=Config(inject_host_prefix=False),
     )
 
-# evaluate feature
-PROJECT: Final[str] = "food"
-FEATURE: Final[str] = "sushi"
 
-entity_id: str = uuid.uuid4().hex
+def evaluate_feature(client, project, feature, entity_id):
+    try:
+        out = client.evaluate_feature(
+            project=project,
+            feature=feature,
+            entityId=entity_id,
+        )
 
-try:
-    out = client.evaluate_feature(
-        project=PROJECT,
-        feature=FEATURE,
-        entityId=entity_id,
-    )
+        print(out.get("reason"))
+        print(out.get("variation"))
+        print(out.get("value"))
 
-    print(out.get("reason"))
-    print(out.get("variation"))
-    print(out.get("value"))
+    except Exception as e:
+        print(e)
 
-except Exception as e:
-    print(e)
+
+if __name__ == "__main__":
+    PROJECT: Final[str] = "food"
+    FEATURE: Final[str] = "sushi"
+    entity_id: str = uuid.uuid4().hex
+
+    args = sys.argv
+    if len(args) > 1:
+        entity_id = args[1]
+
+    client = create_client()
+    evaluate_feature(client, PROJECT, FEATURE, entity_id)
