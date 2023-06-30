ea3ca0792d56a5a608465fe81f6071c7b7fc2798
michimani <michimani210@gmail.com>
2023-07-01 01:02:37 +0900

    use internal logic in evaluation handler #1

--- handler/evaluation.go
+++ handler/evaluation.go
@@ -2,15 +2,11 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
 
-	"github.com/gofrs/uuid"
-
-	"github.com/michimani/evidentlylocal/components"
+	"github.com/michimani/evidentlylocal/internal"
 	"github.com/michimani/evidentlylocal/logger"
-	"github.com/michimani/evidentlylocal/repository"
 	"github.com/michimani/evidentlylocal/types"
 )
 
@@ -33,17 +29,8 @@ func evaluateFeature(w http.ResponseWriter, r *http.Request, l logger.Logger) {
 	project := parts[2]
 	featureName := parts[4]
 
-	feature, err := repository.FeatureRepositoryInstance().Get(project, featureName)
-	if err != nil {
-		l.Error("Failed to get feature", err)
-		http.Error(w, "Resource Not Found", http.StatusNotFound)
-		return
-	}
-
-	// TODO: get entityID from request body
 	request := &types.EvaluateFeatureRequest{}
-	err = json.NewDecoder(r.Body).Decode(request)
-	if err != nil {
+	if err := json.NewDecoder(r.Body).Decode(request); err != nil {
 		l.Error("Failed to decode request body", err)
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
@@ -51,14 +38,12 @@ func evaluateFeature(w http.ResponseWriter, r *http.Request, l logger.Logger) {
 
 	entityID := request.EntityID
 
-	reason, variation, err := components.EvaluateFeature(feature, entityID)
-	if err != nil {
-		l.Error("Failed to evaluate feature", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	reason, variation, eerr := internal.EvaluateFeature(project, featureName, entityID)
+	if eerr != nil {
+		l.Error("Failed to evaluate feature", eerr)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-
-	l.Info(fmt.Sprintf("return variation: %+v", variation))
 
 	res := types.EvaluateFeatureResponse{
 		Details:   "{}",
@@ -67,19 +52,11 @@ func evaluateFeature(w http.ResponseWriter, r *http.Request, l logger.Logger) {
 		Variation: variation.Name,
 	}
 
-	bytes, err := json.Marshal(res)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	bytes, requestID, gerr := internal.GenerateResponseBody(res)
+	if gerr != nil {
+		l.Error("Failed to generate response body", gerr)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
-	}
-
-	requestID := ""
-	uuid, err := uuid.NewV4()
-	if err != nil {
-		l.Error("Failed to generate UUID. Use constant request id.", err)
-		requestID = "xxxxxxxx-0000-0000-0000-xxxxxxxxxxxx"
-	} else {
-		requestID = uuid.String()
 	}
 
 	w.Header().Set("Content-Type", "application/json")
