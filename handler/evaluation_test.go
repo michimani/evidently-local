5850bd2a3a6a2b421c1612c777ced35d258671d3
michimani <michimani210@gmail.com>
2023-07-03 23:45:21 +0900

    add base of almost endpoint #3

--- handler/evaluation_test.go
+++ handler/evaluation_test.go
@@ -20,6 +20,7 @@ func Test_EvaluateFeature(t *testing.T) {
 		name           string
 		reqBody        string
 		reqPath        string
+		method         string
 		expectedStatus int
 		expectedBody   string
 	}{
@@ -27,6 +28,7 @@ func Test_EvaluateFeature(t *testing.T) {
 			name:           "default rule",
 			reqBody:        `{"entityId":"test-entity-id", "evaluateContext":""}`,
 			reqPath:        "/projects/test-project/evaluations/test-feature-1",
+			method:         http.MethodPost,
 			expectedStatus: http.StatusOK,
 			expectedBody:   `{"details":"{}","reason":"DEFAULT (local)","value":{"boolValue":false},"variation":"False"}`,
 		},
@@ -34,6 +36,7 @@ func Test_EvaluateFeature(t *testing.T) {
 			name:           "override rule",
 			reqBody:        `{"entityId":"force-true", "evaluateContext":""}`,
 			reqPath:        "/projects/test-project/evaluations/test-feature-1",
+			method:         http.MethodPost,
 			expectedStatus: http.StatusOK,
 			expectedBody:   `{"details":"{}","reason":"OVERRIDE_RULE (local)","value":{"boolValue":true},"variation":"True"}`,
 		},
@@ -41,15 +44,49 @@ func Test_EvaluateFeature(t *testing.T) {
 			name:           "feature not found",
 			reqBody:        `{"entityId":"test-entity-id", "evaluateContext":""}`,
 			reqPath:        "/projects/test-project/evaluations/not-exists-feature",
+			method:         http.MethodPost,
 			expectedStatus: http.StatusNotFound,
-			expectedBody:   "Resource Not Found\n",
+			expectedBody:   "Not found\n",
 		},
 		{
 			name:           "invalid request body",
 			reqBody:        `///`,
 			reqPath:        "/projects/test-project/evaluations/test-feature-1",
+			method:         http.MethodPost,
 			expectedStatus: http.StatusBadRequest,
-			expectedBody:   "Bad Request\n",
+			expectedBody:   "Bad request\n",
+		},
+		{
+			name:           "method not allowed: GET",
+			reqBody:        `{"entityId":"test-entity-id", "evaluateContext":""}`,
+			reqPath:        "/projects/test-project/evaluations/test-feature-1",
+			method:         http.MethodGet,
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedBody:   "Method not allowed\n",
+		},
+		{
+			name:           "method not allowed: PUT",
+			reqBody:        `{"entityId":"test-entity-id", "evaluateContext":""}`,
+			reqPath:        "/projects/test-project/evaluations/test-feature-1",
+			method:         http.MethodPut,
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedBody:   "Method not allowed\n",
+		},
+		{
+			name:           "method not allowed: PATCH",
+			reqBody:        `{"entityId":"test-entity-id", "evaluateContext":""}`,
+			reqPath:        "/projects/test-project/evaluations/test-feature-1",
+			method:         http.MethodPatch,
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedBody:   "Method not allowed\n",
+		},
+		{
+			name:           "method not allowed: HEAD",
+			reqBody:        `{"entityId":"test-entity-id", "evaluateContext":""}`,
+			reqPath:        "/projects/test-project/evaluations/test-feature-1",
+			method:         http.MethodHead,
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedBody:   "Method not allowed\n",
 		},
 	}
 
@@ -58,7 +95,7 @@ func Test_EvaluateFeature(t *testing.T) {
 			asst := assert.New(tt)
 
 			body := bytes.NewBufferString(c.reqBody)
-			req := httptest.NewRequest(http.MethodPost, c.reqPath, body)
+			req := httptest.NewRequest(c.method, c.reqPath, body)
 
 			w := httptest.NewRecorder()
 
--- handler/project.go
+++ handler/project.go
@@ -16,64 +16,81 @@ func Projects(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	path := r.URL.Path
+	parts := strings.Split(path, "/")
+	l.Info(fmt.Sprintf("%s %s", r.Method, path))
+
+	switch len(parts) {
+	case 2:
+		// GET | POST /projects
+		handleProjects(w, r, l)
+	case 3:
+		// GET | PATCH | DELETE /projects/:project
+		handleSpecificProject(w, r, parts[2], l)
+	case 4:
+		// POST /projects/:project/evaluations
+		// GET | POST /projects/:project/experiments
+		// GET | POST /projects/:project/launches
+		// GET | POST /projects/:project/features
+		handleSomeResources(w, r, parts, l)
+	case 5:
+		// POST /projects/:project/evaluations/:feature
+		// GET | PATCH | DELETE /projects/:project/experiments/:experiment
+		// GET | PATCH | DELETE /projects/:project/launches/:launch
+		// GET | PATCH | DELETE /projects/:project/features/:feature
+		handleSpecificResource(w, r, parts, l)
+	default:
+		http.Error(w, "Not found", http.StatusNotFound)
+	}
+}
+
+func handleProjects(w http.ResponseWriter, r *http.Request, l logger.Logger) {
 	switch r.Method {
-	case http.MethodGet:
-		get(w, r, l)
-	case http.MethodPost:
-		post(w, r, l)
+	case http.MethodGet, http.MethodPost:
+		http.Error(w, "Not implemented", http.StatusNotImplemented)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
-func get(w http.ResponseWriter, r *http.Request, l logger.Logger) {
-	path := r.URL.Path
-	parts := strings.Split(path, "/")
-	l.Info(fmt.Sprintf("GET %s", path))
+func handleSpecificProject(w http.ResponseWriter, r *http.Request, project string, l logger.Logger) {
+	switch r.Method {
+	case http.MethodGet, http.MethodPatch, http.MethodDelete:
+		http.Error(w, "Not implemented", http.StatusNotImplemented)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
 
-	switch len(parts) {
-	case 2:
-		// GET /projects
-		// return all projects, but not supported yet
-		http.Error(w, "Not implemented", http.StatusNotImplemented)
-	case 3:
-		// GET /projects/:project
-		// return the project, but not supported yet
-		http.Error(w, "Not implemented", http.StatusNotImplemented)
-	case 4:
-		// GET /projects/:project/evaluations
-		// return all evaluations of the project, but not supported yet
-		http.Error(w, "Not implemented", http.StatusNotImplemented)
-	case 5:
-		// GET /projects/:project/evaluations/:feature
-		// return the evaluation of the feature, but not supported yet
+func handleSomeResources(w http.ResponseWriter, r *http.Request, pathPart []string, l logger.Logger) {
+	if len(pathPart) != 4 {
+		l.Error("Invalid path: "+r.URL.Path, nil)
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	switch pathPart[3] {
+	case "evaluations", "experiments", "launches", "features":
 		http.Error(w, "Not implemented", http.StatusNotImplemented)
 	default:
 		http.Error(w, "Not found", http.StatusNotFound)
 	}
 }
 
-func post(w http.ResponseWriter, r *http.Request, l logger.Logger) {
-	path := r.URL.Path
-	parts := strings.Split(path, "/")
-	l.Info(fmt.Sprintf("POST %s", path))
+func handleSpecificResource(w http.ResponseWriter, r *http.Request, pathPart []string, l logger.Logger) {
+	if len(pathPart) != 5 {
+		l.Error("Invalid path: "+r.URL.Path, nil)
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
 
-	switch len(parts) {
-	case 2:
-		// POST /projects
-		// create a project, but not supported yet
-		http.Error(w, "Not implemented", http.StatusNotImplemented)
-	case 3:
-		// method not allowed
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	case 4:
-		// POST /projects/:project/evaluations
-		// create an evaluation, but not supported yet
-		http.Error(w, "Not implemented", http.StatusNotImplemented)
-	case 5:
+	switch pathPart[3] {
+	case "evaluations":
 		// POST /projects/:project/evaluations/:feature
-		// evaluate feature
+		// https://docs.aws.amazon.com/cloudwatchevidently/latest/APIReference/API_EvaluateFeature.html
 		evaluateFeature(w, r, l)
+	case "experiments", "launches", "features":
+		http.Error(w, "Not implemented", http.StatusNotImplemented)
 	default:
 		http.Error(w, "Not found", http.StatusNotFound)
 	}
