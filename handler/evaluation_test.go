11ca8edf9a10042c4361656e87582b486a908ec7
michimani <michimani210@gmail.com>
2023-07-05 19:48:38 +0900

    add test for batchEvaluateFeature #11

--- handler/evaluation_test.go
+++ handler/evaluation_test.go
@@ -114,3 +114,106 @@ func Test_evaluateFeature(t *testing.T) {
 		})
 	}
 }
+
+func Test_batchEvaluateFeature(t *testing.T) {
+	testLogger, _ := logger.NewEvidentlyLocalLogger(io.Discard)
+	handler.PrepareForTest(testLogger)
+
+	cases := []struct {
+		name           string
+		reqBody        string
+		reqPath        string
+		method         string
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name:           "one request",
+			reqBody:        `{"requests":[{"entityId":"test-entity-id", "feature": "test-feature-1", "evaluateContext":""}]}`,
+			reqPath:        "/projects/test-project/evaluations",
+			method:         http.MethodPost,
+			expectedStatus: http.StatusOK,
+			expectedBody:   `{"results":[{"details":"{}","entityId":"test-entity-id","feature":"test-feature-1","project":"test-project","reason":"DEFAULT","variation":"False","value":{"boolValue":false}}]}`,
+		},
+		{
+			name:           "two requests (default and override)",
+			reqBody:        `{"requests":[{"entityId":"test-entity-id", "feature": "test-feature-1", "evaluateContext":""},{"entityId":"force-true", "feature": "test-feature-1", "evaluateContext":""}]}`,
+			reqPath:        "/projects/test-project/evaluations",
+			method:         http.MethodPost,
+			expectedStatus: http.StatusOK,
+			expectedBody:   `{"results":[{"details":"{}","entityId":"test-entity-id","feature":"test-feature-1","project":"test-project","reason":"DEFAULT","variation":"False","value":{"boolValue":false}},{"details":"{}","entityId":"force-true","feature":"test-feature-1","project":"test-project","reason":"OVERRIDE_RULE","variation":"True","value":{"boolValue":true}}]}`,
+		},
+		{
+			name:           "with feature not found",
+			reqBody:        `{"requests":[{"entityId":"test-entity-id", "feature": "test-feature-1", "evaluateContext":""},{"entityId":"test-entity-id", "feature": "not-exists-feature", "evaluateContext":""}]}`,
+			reqPath:        "/projects/test-project/evaluations",
+			method:         http.MethodPost,
+			expectedStatus: http.StatusOK,
+			expectedBody:   `{"results":[{"details":"{}","entityId":"test-entity-id","feature":"test-feature-1","project":"test-project","reason":"DEFAULT","variation":"False","value":{"boolValue":false}},{"details":"","entityId":"test-entity-id","feature":"not-exists-feature","project":"test-project","reason":"Feature not found","variation":"","value":null}]}`,
+		},
+		{
+			name:           "invalid request body",
+			reqBody:        `///`,
+			reqPath:        "/projects/test-project/evaluations",
+			method:         http.MethodPost,
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   "Bad request\n",
+		},
+		{
+			name:           "method not allowed: GET",
+			reqBody:        `{"requests":[{"entityId":"test-entity-id", "feature": "test-feature-1", "evaluateContext":""}]}`,
+			reqPath:        "/projects/test-project/evaluations",
+			method:         http.MethodGet,
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedBody:   "Method not allowed\n",
+		},
+		{
+			name:           "method not allowed: PUT",
+			reqBody:        `{"requests":[{"entityId":"test-entity-id", "feature": "test-feature-1", "evaluateContext":""}]}`,
+			reqPath:        "/projects/test-project/evaluations",
+			method:         http.MethodPut,
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedBody:   "Method not allowed\n",
+		},
+		{
+			name:           "method not allowed: PATCH",
+			reqBody:        `{"requests":[{"entityId":"test-entity-id", "feature": "test-feature-1", "evaluateContext":""}]}`,
+			reqPath:        "/projects/test-project/evaluations",
+			method:         http.MethodPatch,
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedBody:   "Method not allowed\n",
+		},
+		{
+			name:           "method not allowed: HEAD",
+			reqBody:        `{"requests":[{"entityId":"test-entity-id", "feature": "test-feature-1", "evaluateContext":""}]}`,
+			reqPath:        "/projects/test-project/evaluations",
+			method:         http.MethodHead,
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedBody:   "Method not allowed\n",
+		},
+		{
+			name:           "invalid request path",
+			reqBody:        `///`,
+			reqPath:        "/projects/test-project",
+			method:         http.MethodPost,
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "Not found\n",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			asst := assert.New(tt)
+
+			body := bytes.NewBufferString(c.reqBody)
+			req := httptest.NewRequest(c.method, c.reqPath, body)
+
+			w := httptest.NewRecorder()
+
+			handler.Exported_batchEvaluateFeature(w, req)
+
+			asst.Equal(c.expectedStatus, w.Code)
+			asst.Equal(c.expectedBody, w.Body.String())
+		})
+	}
+}
